358a7e83bec78193216aecae4a7b6ae3acb8e415
Jorge <jorge_martins@hotmail.es>
2023-04-26 17:34:59 +0200

    Proyecto

--- src/app.js
+++ src/app.js
@@ -1,2 +1,23 @@
-const name = 'world';
-console.log(`Hello ${name}`);
+
+//   var sliderImages = document.querySelectorAll('.slider img');
+//   var current = 0;
+//   var interval = setInterval(changeImage, 5000);
+
+//   function changeImage() {
+//     sliderImages[current].style.opacity = 0;
+//     current = (current + 1) % sliderImages.length;
+//     sliderImages[current].style.opacity = 1;
+//   }
+
+import 'slick-carousel';
+
+$('.slider').slick({
+  dots: true,
+  infinite: true,
+  autoplay:true,
+  autoplaySpeed: 6000,
+  speed: 300,
+  slidesToShow: 1,
+  adaptiveHeight: true,
+  slidesToScroll: 1
+});
